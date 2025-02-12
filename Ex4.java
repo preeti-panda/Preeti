944aa1297516151c02bd6138063d958b87d8578b
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-12 15:09:30 +0530

    Create Ex4.java

---
+++ Ex4.java
@@ -0,0 +1,10 @@
+public class String_Ex4 {
+    public Static void main(String[] args){
+        String s1 = "Sachin";
+        String s2 = "SACHIN";
+        System.out.println(s1.equals(s2));//false
+        System.out.println(s1.equalsIgnoreCase(s2));//true
+        
+    }
+    
+}
