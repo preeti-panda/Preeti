20fb195cce19c7a3827be41954d7d7973ff37597
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-12 15:10:08 +0530

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,16 @@
+public class ex1 {
+   public Static void main(String[] args){
+    String ("Transpose of two matrix");
+    int (val[1,2,3]);
+    int (val[4,5,6]);
+    
+    for(i=1;i<3;i--){
+        void main(S@Override
+        public String toString() {
+            return super.toString();
+        })
+        System.out.println("Print the transpose of matrix1 & matrix2");
+    }
+
+   } 
+}
