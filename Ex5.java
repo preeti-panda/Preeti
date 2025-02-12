d0d836b139e0524f1206a14dcbc4aca1abe9a7cf
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-12 15:08:44 +0530

    Create Ex5.java

---
+++ Ex5.java
@@ -0,0 +1,13 @@
+public class Ex5 {
+    public Static void main(String args[]) {
+        String s1 = "preeti@gmail.com";
+        int = {1,2,3,4}
+
+        
+        System.out.println("Enter the mail id");
+        System.out.println("Enter the Passwoed");
+        Sc.close();  
+    
+    }
+    
+}
