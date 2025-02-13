7451c389a92d995e2edd6dc26cd705312f7fc187
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-13 15:15:41 +0530

    Create javaEx1.java

---
+++ javaEx1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaEx1 {
+    void read(int[] a);
+    
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter"+a.lenght+"Element");
+        for(int i=0;i<a.length;i++)
+        {
+            c[i]=sc.nextInt();
+        }
+        void dispaly(int[] a)
+        {
+            System.out.println("Content Of a Array");
+            for(int x:a)
+        }
+        System.out.println(X);
+        JavaEx1 je=new JavaEx1();
+        read(a);
+
+    }   
