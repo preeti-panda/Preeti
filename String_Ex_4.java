08774c9308949ea64cef045ada3b06f821b56dc6
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-13 15:16:21 +0530

    Create String_Ex_4.java

---
+++ String_Ex_4.java
@@ -0,0 +1,10 @@
+public class String_Ex_4 {
+    public static void main(String[] args){
+        String s = "Preeti";
+        int n = s.length();
+        int count = n;
+        System.out.println("s");
+       
+    }
+    
+}
