e43af22e6e554d128ba061d38a3446604064da1a
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-13 15:14:54 +0530

    Create String_Ex_2.java

---
+++ String_Ex_2.java
@@ -0,0 +1,8 @@
+public class String_Ex_2 {
+    public static void main(String[] args){
+        String s = "Sachin";
+        System.out.println(s.chartAt(0));//S
+        System.out.println(s.chartAt(3));//h
+    }
+    
+}
