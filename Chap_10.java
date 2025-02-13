cba2c24a6d1fbb8bc3162f7bb8c24c890163db8d
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-13 15:14:22 +0530

    Create  Chap_10.java

---
+++ Chap_10.java
@@ -0,0 +1,9 @@
+package Chap_10_java_Strings;
+public class ex10 {
+    public static void main(String[] args){
+        String s = "Sachin";
+        System.out.println(s);
+        
+    }
+    
+}
