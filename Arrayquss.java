8fbb0bd610b198c5c7b12fbc97549073ce6c8f71
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-20 10:52:38 +0530

    Create Arrayquss.java

---
+++ Arrayquss.java
@@ -0,0 +1,14 @@
+public class Arrayqu {
+    public static void main(String args[]){
+        
+            int[] arr={1,4,5};
+            int sum = 0;
+            for (int i=0;i<arr.length;i++){
+                sum = sum + arr[i];
+            }
+            System.out.println(sum);
+
+        
+    }
+    
+}
