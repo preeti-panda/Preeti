cdf638bff92e87914d6254340caa12bb5e176ac2
Preeti Prangya Panda <164388733+preeti-panda@users.noreply.github.com>
2025-02-24 11:01:49 +0530

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,9 @@
+public class Question1 {
+    public static void main(String[] args) {
+        
+        String name1 = "Preeti";
+        String name2 = "Panda";
+        String fullName = name1 + " " + name2;
+        System.out.println("Full Name: " + fullName);
+    }
+}
